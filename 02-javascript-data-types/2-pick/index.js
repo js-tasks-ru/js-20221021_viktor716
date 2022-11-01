2ebebde0dfb638dfc2b2eb82360a798d6114be9f
Mankevich Victor <jumperfromcloset@gmail.com>
2022-11-01 22:22:43 +0700

    2-pick and 3-omit correction

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -7,8 +7,15 @@
 export const pick = (obj, ...fields) => {
   const newObjectToReturn = {};
   
+  // вариант решения с занятия
+  /*for (const [key, value] of Object.entries(obj)) {
+    if (fields.includes(key)) {
+      newObjectToReturn[key] = value;
+    }
+  }*/
+
   for (const key of fields) {
-    if(key in obj) {
+    if (obj.hasOwnProperty(key)) {
       newObjectToReturn[key] = obj[key];
     }
   }
--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -7,9 +7,9 @@
 export const omit = (obj, ...fields) => {
   const newObjectToReturn = {};
 
-  for (const key in obj) {
+  for (const [key, value] of Object.entries(obj)) {
     if (!fields.includes(key)) {
-      newObjectToReturn[key] = obj[key];
+      newObjectToReturn[key] = value;
     }
   }
 
