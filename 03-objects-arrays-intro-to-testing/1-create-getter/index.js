bd61c106670f700656f97eaa05ed9d02551932f1
Mankevich Victor <jumperfromcloset@gmail.com>
2022-11-05 16:37:25 +0700

    create-getter and trim-symbols solved

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,19 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const separatedPath = path.split('.');
 
+  return (obj) => {
+    let resultValue = obj;
+    
+    for (const pathPart of separatedPath) {
+      if (resultValue === undefined) {
+        break
+      };
+
+      resultValue = resultValue[pathPart];
+    }
+ 
+    return resultValue;
+  }
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,28 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === undefined) {
+    return string;
+  }
 
+  if (size === 0) {
+    return '';
+  }
+
+  const arr = [...string];  
+  let currentLength = 0;
+  let resultString = '';
+ 
+  arr.forEach((item, index, array) => {
+    if(array[index] !== array[index-1]) {
+      currentLength = 0;
+    }
+
+    if(currentLength < size) {
+      resultString += item;
+      currentLength++
+    }
+  });
+
+  return resultString;
 }
