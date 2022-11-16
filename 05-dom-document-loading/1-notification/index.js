0257916565e7b967bbc24a6fe1dddf4297667f7b
Mankevich Victor <jumperfromcloset@gmail.com>
2022-11-17 04:46:56 +0700

    notification message and table v1

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,65 @@
 export default class NotificationMessage {
+  static currentMessage;
+  element;
+  currentTimeout;
 
+  constructor(message = "", {
+    duration = 1000,
+    type = "success"
+  } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.render();
+  }
+
+  getTemplate() {
+    return `
+    <div class="notification ${this.type}" style="--value:${this.duration/1000}s">
+      <div class="timer"></div>
+      <div class="inner-wrapper">
+        <div class="notification-header">${this.type}</div>
+        <div class="notification-body">
+          ${this.message}
+        </div>
+      </div>
+    </div>
+    `
+  }
+
+  show(placeToShow = document.body) {
+    if (NotificationMessage.currentMessage) {
+      NotificationMessage.currentMessage.remove();
+    }
+
+    placeToShow.append(this.element);
+    NotificationMessage.currentMessage = this;
+
+    this.currentTimeout = setTimeout(() => {
+      this.remove();
+    }, this.duration);
+  }
+
+  render() {
+    const newElement = document.createElement("div");
+
+    newElement.innerHTML = this.getTemplate();
+
+    this.element = newElement.firstElementChild;
+  }
+
+  remove() {
+    if(this.element) {
+      this.element.remove();
+    }
+
+    this.currentTimeout = null;
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    NotificationMessage.currentMessage = null;
+  }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,147 @@
 export default class SortableTable {
-  constructor(headerConfig = [], data = []) {
+  subElements = {};
+  element;
 
+  constructor(headerConfig = [], data = []) {
+    this.headerConfig = headerConfig;
+    this.data = data;
+
+    this.render();
+  }
+
+  getTemplate() {
+    return `
+    <div class="sortable-table">
+      ${this.getHeader()}
+      ${this.getBody()}
+    </div>
+    `;
+  }
+
+  getHeader() {
+    return `
+    <div data-element="header" class="sortable-table__header sortable-table__row">
+      ${this.getAllHeaderCells()}
+    </div>
+    `;
+  }
+
+  getAllHeaderCells() {
+    return this.headerConfig.map((item) => this.getHeaderCell(item)).join('');
+  }
+
+  getHeaderCell(item){
+    return `
+    <div class="sortable-table__cell" data-id="${item.id}" data-sortable="${item.sortable}">
+      <span>${item.title}</span>
+      <span data-element="arrow" class="sortable-table__sort-arrow">
+        <span class="sort-arrow"></span>
+      </span>
+    </div>
+    `;
+  }
+
+  getBody() {
+    return `
+    <div data-element="body" class="sortable-table__body">
+      ${this.getAllBodyRows(this.data)}
+    </div>
+    `;
+  }
+
+  getAllBodyRows(data = []) {
+    return data.map((item) => {
+      return `
+      <a href="/products/${item.id}" class="sortable-table__row">
+        ${this.getBodyRow(item)}
+      </a>
+      `;
+    }).join('');
+  }
+
+  getBodyRow(item) {
+    const cells = this.headerConfig.map(({id, template}) => {
+      return {
+        id,
+        template
+      };
+    });
+
+    return cells.map(({id, template}) => {
+      if (template) {
+        return template(item[id]);
+      } else {
+        return `<div class="sortable-table__cell">${item[id]}</div>`;
+      }
+    }).join('');
+  }
+
+  render() {
+    const newElement = document.createElement("div");
+
+    newElement.innerHTML = this.getTemplate();
+
+    const tableElement = newElement.firstElementChild;
+
+    this.element = tableElement;
+
+    this.subElements = this.getSubElements(tableElement);
+  }
+
+  getSubElements(tableElement) {
+    const subElements = {};
+    const dataElements = tableElement.querySelectorAll('[data-element]');
+
+    for (const subElement of dataElements) {
+      const name = subElement.dataset.element;
+      subElements[name] = subElement;
+    }
+
+    return subElements;
+  }
+
+  remove() {
+    if(this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    this.subElements = {};
+  }
+
+  sort(fieldValue, orderValue) {    
+    const tableColumns = this.element.querySelectorAll('.sortable-table__cell[data-id]');
+    const sortingColumn = this.element.querySelector(`.sortable-table__cell[data-id="${fieldValue}"]`);
+    const newSortedData = this.dataSort(fieldValue, orderValue);
+
+    tableColumns.forEach((column) => {
+      column.dataset.order = '';
+    });
+    sortingColumn.dataset.order = orderValue;
+
+    this.subElements.body.innerHTML = this.getAllBodyRows(newSortedData);
+  }
+
+  dataSort(fieldValue, orderValue) {
+    const arr = [...this.data];
+    const sortingColumn = this.headerConfig.find((item) => item.id === fieldValue);
+    const { sortType } = sortingColumn;
+    
+    const sortCollator = new Intl.Collator('ru', { caseFirst: 'upper' } );
+    const sortDirection = (orderValue === 'asc') ? 1 : (orderValue === 'desc') ? -1 : null;
+
+    return arr.sort((a, b) => {
+      if (sortType === 'number') {
+        return (a[fieldValue] - b[fieldValue]) * sortDirection;
+      } else
+      if (sortType === 'string') {
+        return sortCollator.compare(a[fieldValue], b[fieldValue]) * sortDirection;
+      } else {
+        throw new Error(`Sorting type ${sortType} is not supported`);
+      }
+    });
   }
 }
-
